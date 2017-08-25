547b15bed0b20aefb977206d2f6332d83b91668f
unknown <dwgames@gmail.com>
2017-08-24 18:32:50 -0700

    Working on Update

---
+++ ChinookConsoleApp/UpdateEmployee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookConsoleApp
+{
+    class UpdateEmployee
+    {
+        public void Update()
+        {
+            Console.WriteLine("Enter First Name:");
+            var x = Console.ReadLine();
+            Console.WriteLine("Enter last name:");
+            var y = Console.ReadLine();
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    var rowsAffected = connection.Execute("Insert into Employee(FirstName, LastName) " +
+                                                          "Values(@FirstName, @LastName)",
+                        new { FirstName = x, LastName = y });
+
+                    Console.WriteLine(rowsAffected != 1 ? "Add Failed" : "Success!");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("You done messed up");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+
+
+                Console.WriteLine("Press enter to return to the menu.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
